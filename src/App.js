9a50451ac57f32dbc5a07d45db40d52e7c823439
agus0jeda <oagusj@gmail.com>
2023-09-16 16:07:31 -0300

    clases completadas

--- src/App.js
+++ src/App.js
@@ -1,23 +1,156 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { v4 as uuid } from "uuid";
 import './App.css';
+import Header from './components/Header/Header';
+import Formulario from './components/Formulario/Formulario';
+import MiOrg from './components/MiOrg/MiOrg';
+import Equipo from './components/Equipo/Equipo';
+import Footer from './components/Footer/Footer';
 
 function App() {
+
+  const [ mostrarFormulario, actualizarMostrar ] = useState(false)
+  const [ colaboradores, setColaboradores ] = useState([{
+    id: uuid(),
+    equipo: "Front End",
+    foto:"https://github.com/Agus0jeda.png",
+    nombre: "Ojeda Agustin",
+    puesto: "Estudiante",
+    fav: true
+  },
+  {
+    id: uuid(),
+    equipo: "Programación",
+    foto:"https://github.com/harlandlohora.png",
+    nombre: "Harland Lohora",
+    puesto: "Instructor",
+    fav: false
+  }
+])
+  const [equipos, setEquipos ] = useState([
+    {
+      id: uuid(),
+      titulo: "Programación",
+      colorPrimario: "#57C278",
+      colorSecundario: "#D9F7E9"
+    },
+    {
+      id: uuid(),
+      titulo: "Front End",
+      colorPrimario: "#82CFFA",
+      colorSecundario: "#E8F8FF"
+    },
+    {
+      id: uuid(),
+      titulo: "Data Science",
+      colorPrimario: "#A6D157",
+      colorSecundario: "#F0F8E2"
+    },
+    {
+      id: uuid(),
+      titulo: "Devop",
+      colorPrimario: "#E06B69",
+      colorSecundario: "#FDE7E8"
+    },
+    {
+      id: uuid(),
+      titulo: "UX y Diseño",
+      colorPrimario: "#DB6EBF",
+      colorSecundario: "#FAE9F5"
+    },
+    {
+      id: uuid(),
+      titulo: "Movil",
+      colorPrimario: "#FFBA05",
+      colorSecundario: "#FFF5D9"
+    },
+    {
+      id: uuid(),
+      titulo: "Innovación y Gestión",
+      colorPrimario: "#FF8A29",
+      colorSecundario: "#FFEEDF"
+    },
+  ])
+
+  //ternario --> condicion ? seMuetra : noSeMuestra
+  //otra opcion--> condidion && seMuestra
+      // * --> {mostrarFormulario && <Formulario /> }
+
+  const cambiarMostrar = () => {
+    actualizarMostrar(!mostrarFormulario)
+  }
+
+  //Registrar colaborador
+  const registrarColaborador = (colaborador) => {
+    console.log("Nuevo colaborador", colaborador)
+    //Spread operator
+    setColaboradores([...colaboradores, colaborador])
+  }
+
+  //Eliminar colaborador
+  const eliminarColaborador = (id) =>{
+    console.log("Eliminar colaborador", id)
+    const nuevosColaboradores = colaboradores.filter((colaborador) => colaborador.id !== id)
+    setColaboradores(nuevosColaboradores)
+  }
+
+  //Actualizar color de equipo
+  const actualizarColor = (color,id) => {
+    console.log("Actualizar: ", color,id)
+    const equiposActualizados = equipos.map((equipo) => {
+      if(equipo.id === id){
+        equipo.colorPrimario = color
+      }
+      return equipo
+    })
+    setEquipos(equiposActualizados)
+  }
+
+  //Crear equipo
+  const crearEquipo = (nuevoEquipo) => {
+    console.log(nuevoEquipo)
+    setEquipos([...equipos, {...nuevoEquipo, id: uuid()}])
+  }
+
+  const like = (id) => {
+    console.log("like", id)
+    const colaboradoresActualizados = colaboradores.map((colaborador) => {
+      if(colaborador.id === id){
+        colaborador.fav = !colaborador.fav
+      }
+      return colaborador
+    })
+
+    setColaboradores(colaboradoresActualizados)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      { 
+        mostrarFormulario === true ? <Formulario
+          equipos={equipos.map((equipo) => equipo.titulo)}
+          registrarColaborador={registrarColaborador}
+          crearEquipo={crearEquipo}
+          /> : <div></div>
+      }
+      
+      <MiOrg cambiarMostrar = { cambiarMostrar } />
+      
+      {
+        equipos.map((equipo) => <Equipo 
+          datos={equipo} 
+          key={equipo.titulo} 
+          colaboradores={colaboradores.filter(colaborador => colaborador.equipo === equipo.titulo)}
+          eliminarColaborador={eliminarColaborador}
+          actualizarColor={actualizarColor}
+          like={like}
+          />
+        )
+      }
+
+      <Footer />
+
     </div>
   );
 }
---
+++ src/components/Formulario/Formulario.js
@@ -0,0 +1,96 @@
+import { useState } from "react";
+import "./Formulario.css";
+import Campo from "../Campo/Campo";
+import ListaOpciones from "../ListaOpciones/ListaOpciones";
+import Boton from "../Boton/Boton";
+
+const Formulario = (props) => {
+
+    const [ nombre, setNombre ] = useState("")
+    const [ puesto, setPuesto ] = useState("")
+    const [ foto, setFoto ] = useState("")
+    const [ equipo, setEquipo ] = useState("")
+
+    const [titulo, setTitulo ] = useState("")
+    const [color, setColor ] = useState("")
+
+    const { registrarColaborador, crearEquipo } = props
+    // const {}
+
+    const manejarEnvio = (event) => {
+        event.preventDefault()
+        console.log("Manejar el envio")
+        let datosAEnviar = {
+            nombre : nombre,
+            puesto : puesto,
+            foto : foto,
+            equipo : equipo
+        }
+        registrarColaborador(datosAEnviar)
+    }
+
+    const manejarNuevoEquipo = (e) => {
+        e.preventDefault()
+        crearEquipo({ titulo, colorPrimario: color })
+    }
+
+    return <section className="formulario">
+        <form onSubmit={manejarEnvio}>
+            <h2>Rellena el formulario para crear el colaborador.</h2>
+            <Campo 
+                titulo="Nombre" 
+                placeholder="Ingresar nombre"  
+                required 
+                valor = {nombre} 
+                setValor = {setNombre} 
+            />
+            <Campo 
+                titulo="Puesto" 
+                placeholder="Ingresar puesto" 
+                required 
+                valor = {puesto} 
+                setValor = {setPuesto} 
+            />
+            <Campo 
+                titulo="Foto" 
+                placeholder="Ingresar enlace de foto" 
+                required 
+                valor = {foto} 
+                setValor = {setFoto} 
+            />
+            <ListaOpciones 
+                valor = {equipo}
+                setEquipo = {setEquipo}
+                equipos={props.equipos}
+            />
+            <Boton>
+                Crear
+            </Boton>
+        </form>
+
+        <form onSubmit={manejarNuevoEquipo}>
+            <h2>Rellena el formulario para crear el equipo.</h2>
+            <Campo 
+                titulo="Titulo" 
+                placeholder="Ingresar titulo"  
+                required 
+                valor = {titulo} 
+                setValor = {setTitulo} 
+            />
+            <Campo 
+                titulo="Color" 
+                placeholder="Ingresar el color en Hex" 
+                required 
+                valor = {color} 
+                setValor = {setColor} 
+                type="color"
+            />
+            <Boton>
+                Registrar Equipo
+            </Boton>
+        </form>
+    </section>
+}
+
+
+export default Formulario;
